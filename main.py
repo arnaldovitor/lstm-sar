c6fdac6e0fabfe6dd4446e32e3e5d9c59991177d
felipebattist <felipebattist@gmail.com>
2021-05-24 15:34:54 -0300

    Steps com divisão entre treino e teste.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from tensorflow.python.keras.layers import Dropout
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
 import util
 import numpy as np
 import tensorflow as tf
@@ -7,30 +10,43 @@ from tensorflow.keras.layers import Dense
 
 if __name__ == '__main__':
     sequence = util.separe_column(r'monitoramento-cpu.txt', 'usr')
-    train, test = util.split_sets(sequence, 0.70)
 
     n_steps = 3
 
-    X, y = util.split_sequence(train.values.tolist(), n_steps)
-    X_test, y_test = util.split_sequence(test.values.tolist(), n_steps)
+    X, y = util.split_sequence(sequence.values.tolist(), n_steps)
 
     #converte os np array de string -> float
     X = X.astype(np.float)
     y = y.astype(np.float)
-    X_test = X_test.astype(np.float)
-    y_test = y_test.astype(np.float)
+
 
     #mudar axis do vetor
     n_features = 1
     X = X.reshape((X.shape[0], X.shape[1], n_features))
-    X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], n_features))
 
-    print(X.shape, X_test.shape, y.shape, y_test.shape)
-    print(X[0], y[0])
-    breakpoint()
+
     model = Sequential()
-    model.add(LSTM(50, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(LSTM(10, activation='relu', input_shape=(n_steps, n_features)))
+    model.add(Dropout(0.2))
     model.add(Dense(1))
-    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss='mse', metrics=['mse', 'acc'])
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss='mse', metrics=['mse'])
 
-    history = model.fit(X, y, epochs=100, validation_data=(X_test, y_test), verbose=1)
+    history = model.fit(X, y, epochs=20, verbose=1)
+
+    resultados = model.evaluate(X, y)
+
+    pred = model.predict(X)
+
+    print("loss, mse, acc = ", resultados)
+
+    for i in range(len(pred)):
+        print(pred[i], y[i])
+
+
+
+plt.plot(y, label = 'Comparação entre Y_true e Y_pred', color = 'blue')
+plt.plot(pred, label = 'Y_pred', color = 'red')
+plt.xlabel('Número de neurônios na hidden layer.')
+plt.title("n_steps: " + str(n_steps))
+plt.legend()
+plt.show()
